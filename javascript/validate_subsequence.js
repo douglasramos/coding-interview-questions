3483dc78cf2737aa47d92c644e8acb043bbf5a6d
Douglas Ramos <douglasramos.drc@gmail.com>
2023-02-14 23:11:18 -0300

    comment adjust

--- javascript/validate_subsequence.js
+++ javascript/validate_subsequence.js
@@ -1,5 +1,5 @@
 // Given two non-empty arrays of intergers, write a function that determines whether the second array is a subsequence of the firt one
-// A subsequence of an array is a set of nunmbers that aren't necessarily adjacent in the array but that are in the same order as they apper in the array
+// A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that are in the same order as they apper in the array
 // For instance, the numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 4], and so do the number [2, 4].
 // Note that a single number in an array and the array itself are both valid subsequences of the array
 
